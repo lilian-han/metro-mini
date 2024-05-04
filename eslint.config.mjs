c9ad02c9f38a9fd65d75d13974489d3b75efbf32
韩莉萍 <liping.han@bangdao-tech.com>
2024-05-04 16:33:43 +0800

    feat: 提交文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,49 +1,46 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-
-export default [
-  {
-    root: true,
-    env: {
-      node: true,
-    },
-    globals: {
-      API: "readonly",
-      my: "readonly",
-      uni: "readonly",
-      getCurrentPages: "readonly",
-    },
-    extends: [
-      "eslint-config-prettier", // 解决 eslint 和 prettier 冲突
-      "eslint:recommended",
-      "plugin:vue/vue3-recommended",
-      "plugin:vue/vue3-essential",
-      "plugin:prettier/recommended",
-      './.eslintrc-auto-import.json',
-  ],
-    rules: {
-      "no-var": "error",
-      "prettier/prettier": "error",
-      // 修改 eslint-plugin-vue 8 组件必须驼峰
-      "vue/multi-word-component-names": [
-        "error",
-        {
-          ignores: ["index"],
-        },
-      ],
-      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    },
-    languageOptions: { globals: globals.browser },
-    parser: "@typescript-eslint/parser", // 使用@typescript-eslint/parser这个解析器进行语法解析
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
+module.exports = {
+  root: true,
+  env: {
+    node: true,
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-];
+  plugins: ['vue', '@typescript-eslint'],
+  extends: [
+    'eslint-config-prettier', // 解决 eslint 和 prettier 冲突
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+    './.eslintrc-auto-import.json',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    requireConfigFile: false,
+  },
+  globals: {
+    API: 'readonly',
+    my: 'readonly',
+    uni: 'readonly',
+    getCurrentPages: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['src/pages/**/*.vue', 'src/utils/alipayLogger.js'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
+    },
+  ],
+  rules: {
+    'no-var': 'error',
+    'prettier/prettier': 'error',
+    // 修改 eslint-plugin-vue 8 组件必须驼峰
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index'],
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
---
+++ stylelint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  customSyntax: 'postcss-html',
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue', 'stylelint-config-prettier'],
+  // fix  Unexpected unknown at-rule "@mixin" https://github.com/stylelint/stylelint/issues/3190
+  plugins: ['stylelint-scss'],
+  ignoreFiles: ['node_modules/**/*.scss'],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'font-family-no-missing-generic-family-keyword': null,
+    'unit-no-unknown': [true, { ignoreUnits: 'rpx' }],
+    'import-notation': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-class-pattern': null,
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
+  },
+};
